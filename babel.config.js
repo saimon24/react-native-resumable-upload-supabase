714b53d2fe9c2b14af96cfc981cf651ba65f7945
Simon Grimm <saimon@devdactic.com>
2023-07-12 12:13:36 +0200

    Update TS.

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: ['babel-preset-expo'],
     plugins: [
-      "@babel/plugin-proposal-export-namespace-from",
-      "react-native-reanimated/plugin",
-      require.resolve("expo-router/babel"),
+      // Required for expo-router
+      'expo-router/babel',
     ],
   };
 };
